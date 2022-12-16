676b95adbdb6e4fa183f08bcb928b746fe6f0915
alexpxrad <alexpxrad@gmail.com>
2022-12-15 20:24:32 -0500

    fixed function in API

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const graphqlHttp = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
+app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
             type RootQuery {
                     events: [String!]!
@@ -21,7 +21,18 @@ app.use('/graphql', graphqlHttp({
                 mutation: RootMutation
             }
     `),
-    rootValue: {}
-}));
+    rootValue: {
+        events: () => {
+            return [ 'Romantic Cooking', 'Sailing', 'All-Night Coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+
+})
+);
 
 app.listen(4000);
